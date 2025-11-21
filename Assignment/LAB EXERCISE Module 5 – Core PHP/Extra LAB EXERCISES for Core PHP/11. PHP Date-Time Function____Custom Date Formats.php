6d6b7bbe6bbc443f185b86ab7da8deedafdc3143
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:28 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/Extra LAB EXERCISES for Core PHP/11. PHP Date-Time Function____Custom Date Formats.php
@@ -0,0 +1,17 @@
+
+
+<?php
+
+date_default_timezone_set("Asia/Kolkata");
+
+
+echo "<h3> CUSTOM DATE FORMATS <h3>";
+
+
+echo "FORMAT (Y-m-d): " . date("Y-m-d") . "<br>";
+echo "FORMAT (d/m/y): " . date("d/m/Y") . "<br>";
+echo "DAY OF THE WEEK (1): " . date("1") . "<br>";
+echo "FULL FORMAT (F js Y): " . date("F js Y") . "<br>";
+echo "TIME (H:i:s A): " . date("h:i:s A") . "<br>";
+
+?>
