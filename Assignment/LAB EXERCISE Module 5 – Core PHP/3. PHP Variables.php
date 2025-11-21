778e88bbd754ec850845a0b70f64c6e81d436360
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:03 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/3. PHP Variables.php
@@ -0,0 +1,12 @@
+<?php
+
+$age=22;               // int
+$name="kunj_prajapati";    //string
+$price=5000.20;        //float
+$thisAvailable=true;   //boolean
+
+
+echo "int (Age):" .$age. "<br>";
+echo "String (Name):" .$name. "<br>";
+echo "Boolean (ThisAvailable):" .($thisAvailable ? "true": "false"). "<br>"; 
+?>
