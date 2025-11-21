a4d99012b9d166c2aab546b22b946036b797280a
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:12 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/9. Switch Case and Ternary Operator  3. Color Selector.php
@@ -0,0 +1,26 @@
+<?php
+
+$color="red";
+
+switch(strtolower($color))
+{
+	case "red":
+	echo "You selected Red.";
+	break;
+	
+	case "green":
+	echo "You selected Green.";
+	break;
+	
+	case "blue":
+	echo "You selected Blue.";
+	break;
+	
+	default:
+	echo "Unknown color !";
+	
+}
+
+
+
+?>
