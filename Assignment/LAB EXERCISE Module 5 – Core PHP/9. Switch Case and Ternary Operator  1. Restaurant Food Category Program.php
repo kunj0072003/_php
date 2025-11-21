775a3e965a020a4e9044a30b44d8b78a2f1bb605
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:10 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/9. Switch Case and Ternary Operator  1. Restaurant Food Category Program.php
@@ -0,0 +1,28 @@
+<?php
+
+$choice=1;
+{
+	switch($choice)
+	{
+		case 1:
+		echo "Category: Starter<br>";
+		echo "Dish: Spring Rolls";
+		break;
+		
+		case 2;
+		echo "Category: Main Course<br>";
+		echo "Dish: Paneer Butter Masala";
+		break;
+		
+		case 2;
+		echo "Category: Dessert<br>";
+		echo "Dish: Gulab Jamun";
+		break;
+		
+		default:
+		echo "invalid selection !!";
+	}
+}
+
+
+?>
