0a0169c04c9e3bc8d651801a6550a85df2c02746
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:24 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/Extra LAB EXERCISES for Core PHP/1. PHP Syntax_____PHP Comments.php
@@ -0,0 +1,12 @@
+<?php
+// This is a single-line comment
+
+# This is also a single-line comment using hash
+
+/*
+This is a multi-line comment
+You can write across multiple lines
+*/
+
+echo "Comments are ignored by PHP and not shown in output.";
+?>
