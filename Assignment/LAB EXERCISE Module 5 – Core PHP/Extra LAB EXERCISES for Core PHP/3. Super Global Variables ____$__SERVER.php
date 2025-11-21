d3eb9f4bde31e6d0c64d90010ee89f6e185fa639
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:37 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/Extra LAB EXERCISES for Core PHP/3. Super Global Variables ____$__SERVER.php
@@ -0,0 +1,22 @@
+
+
+
+
+<?php
+
+
+echo "<h3> SERVER Environment Details </h3>";
+echo "PHP Self: " . $_SERVER['PHP_SELF'] . "<br>";
+echo "HTTP Name: " . $_SERVER['SERVER_NAME'] . "<br>";
+echo "HTTP Host: " . $_SERVER['HTTP_HOST'] . "<br>";
+echo "HTTP User Agent: " . $_SERVER['HTTP_USER_AGENT'] . "<br>";
+echo "Script Name: " . $_SERVER['SCRIPT_NAME'] . "<br>";
+echo "Request Method: " . $_SERVER['REQUEST_METHOD'] . "<br>";
+
+?>
+
+
+
+
+
+
