e88813c87cd62020bc499d82abd1ae23b0a8448e
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:33 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/Extra LAB EXERCISES for Core PHP/2. PHP Variables___Variable Variables.php
@@ -0,0 +1,10 @@
+<?php
+
+$name="city";
+$city="Mumbai";
+
+echo "Value of \$name: $name<br>";
+echo "Value of \$city: $city<br>";
+echo "Value using variable variable \$\$name: " . $$name . "<br>";
+
+?>
