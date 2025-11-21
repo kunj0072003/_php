9723e26c88a8e27d9231bbba6f2fb801e3fa77cb
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:17:53 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/11. PHP Array and Array Functions   3. ___.php
@@ -0,0 +1,16 @@
+<?php
+
+$user = array(
+    "name" => "kapil Sharma",
+    "email" => "kapil@009.com",
+    "age" => 44
+);
+
+echo "User Details:<br>";
+echo "Name: " . $user["name"] . "<br>";
+echo "Email: " . $user["email"] . "<br>";
+echo "Age: " . $user["age"];
+
+
+
+?>
