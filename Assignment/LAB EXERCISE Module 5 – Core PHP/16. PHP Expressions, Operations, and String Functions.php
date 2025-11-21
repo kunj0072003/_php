016490053b3f6b81690a2c98ed6bdc20afb33b48
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:02 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/16. PHP Expressions, Operations, and String Functions.php
@@ -0,0 +1,34 @@
+<?php
+
+//16. PHP Expressions, Operations, and String Functions 
+
+// Write a script to perform various string operations like concatenation, substring extraction, and string length determination. 
+
+//Arithmetic Operations
+/*
+
+$K=120;
+$U=234;
+
+ECHO $K+$U;
+ECHO "<br>";
+ECHO $K%$U;
+ECHO "<br>";
+ECHO $K**$U;
+*/
+
+
+//Logical Operations
+
+$n=14;
+$j=-7;
+
+echo ($x>0 && $y>0); // false
+echo ($x>0 || $y>0); // true
+echo !($x>0);          // false
+
+
+
+
+
+?>
