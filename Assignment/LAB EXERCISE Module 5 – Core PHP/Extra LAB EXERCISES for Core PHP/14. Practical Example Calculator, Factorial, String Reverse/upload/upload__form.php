131ab9a752fa4467c1abdb4b68b7ee5fd1a8824c
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:19:01 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/Extra LAB EXERCISES for Core PHP/14. Practical Example Calculator, Factorial, String Reverse/upload/upload__form.php
@@ -0,0 +1,21 @@
+<html>
+
+
+<head>
+	<title> FILE UPLOAD </title>
+</head>
+
+
+<body>
+
+	<h3> UPLOAD a File </h3>
+	<form action="upload__process.php" method="post" enctype="multipart/form-data">
+	<input type="file" name="uploadfile">
+	<button type="submit">Upload</button>
+	
+	</form>
+
+</body>
+
+
+</html>
