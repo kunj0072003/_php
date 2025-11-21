d906f70f57f8babcceea8ac134a8db145f01aeaa
kunj0072003 <kunjprajapati007@gmail.com>
2025-11-21 20:18:45 +0530

    Upload 170 files via GitHub Uploader by Mubashir Hassan

---
+++ Assignment/LAB EXERCISE Module 5 – Core PHP/Extra LAB EXERCISES for Core PHP/7. Practical Example Calculator and Day Finde____Date Finder with Time Zone.php
@@ -0,0 +1,20 @@
+<?php
+
+
+date_default_timezone_set("Asia/Kolkata");
+
+$day=date("1");
+
+echo "<h3> TODAY IS: $day</h3>";
+
+if($day=="SUNDAY")
+{
+	ECHO "🎉 Happy Sunday!";
+}
+ELSE
+{
+	ECHO "Have a great $day ! ";
+}
+
+
+?>
